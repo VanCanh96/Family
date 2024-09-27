86a5342171ff7630690044e3e910bb07fa5ab4ba
canhdv <avancanh@gmail.com>
2024-09-27 13:40:11 +0700

    first commit

---
+++ Mon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mon/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+setInterval(() => {
+    const currentDate = new Date();
+    const dob = new Date(2024, 10 - 1, 3);
+
+    // Tính khoảng cách giữa hai ngày (mili giây)
+    let diffInMs = Math.abs(dob - currentDate);
+
+    // Chuyển đổi mili giây thành số ngày, giờ, phút
+    let diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    let diffInHours = Math.floor((diffInMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let diffInMinutes = Math.floor((diffInMs % (1000 * 60 * 60)) / (1000 * 60));
+    let diffInSeconds = Math.floor((diffInMs % (1000 * 60)) / (1000));
+
+    $("#countDate").text(`${diffInDays} ngày ${diffInHours} giờ ${diffInMinutes} phút ${diffInSeconds} giây`);
+}, 1000);
